06ab4233971f1c638bf465d83fc6dc518a2b41c3
John Dany Osorio R <johnoso27@hotmail.com>
2015-08-22 12:46:41 -0500

    MOD
    Se pasa al centralizador las dto respuesta.

---
+++ projectCentralizador/projectCentralizador-ejb/src/main/java/co/uniandes/edu/service/Response/RespuestaMetadata.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.uniandes.edu.service.Response;
+
+import co.uniandes.edu.ecos.dto.MetadataTipoDocumentoDto;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author John Dany
+ */
+public class RespuestaMetadata {
+
+    public RespuestaMetadata() {
+        this.tiposDocumentoMetaData = new ArrayList<>();
+    }
+
+    public RespuestaMetadata(List<MetadataTipoDocumentoDto> tiposDocumentoMetaData) {
+        this.tiposDocumentoMetaData = tiposDocumentoMetaData;
+    }
+    
+    private List<MetadataTipoDocumentoDto> tiposDocumentoMetaData;
+
+    public List<MetadataTipoDocumentoDto> getTiposDocumentoMetaData() {
+        return tiposDocumentoMetaData;
+    }
+
+    public void setTiposDocumentoMetaData(List<MetadataTipoDocumentoDto> tiposDocumentoMetaData) {
+        this.tiposDocumentoMetaData = tiposDocumentoMetaData;
+    }
+    
+    
+    
+}
