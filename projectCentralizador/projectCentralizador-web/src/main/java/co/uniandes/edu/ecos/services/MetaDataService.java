9ed8f6090f4b85270760e0e3a05a882b769cfa30
angela <angela.suarez.torres@gmail.com>
2015-08-22 14:36:34 -0500

    commit Servicios web

---
+++ projectCentralizador/projectCentralizador-web/src/main/java/co/uniandes/edu/ecos/services/MetaDataService.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.uniandes.edu.ecos.services;
+
+import co.uniandes.edu.ecos.negocio.IMetadataServiceLocal;
+import co.uniandes.edu.service.Response.RespuestaMetadata;
+import javax.ejb.EJB;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author John Dany
+ */
+@WebService(serviceName = "ServicioWebCentralizador")
+public class MetaDataService {
+
+    @EJB
+    private IMetadataServiceLocal metaDataServiceBean;
+
+    /**
+     * This is a sample web service operation
+     * @param txt
+     * @return 
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hola " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     *
+     * @return
+     */
+    @WebMethod(operationName = "obtenerMetadataTiposDocumentos")
+    public RespuestaMetadata obtenerMetadataTiposDocumentos() {
+        //TODO write your implementation code here:
+       RespuestaMetadata metadata=metaDataServiceBean.obtenerTiposDocumentoMetadata();     
+      
+        return metadata;
+    }
+}
