dba10fbac2062f3c5ff8f25fdfdb58f352439c6a
John Dany Osorio R <johnoso27@hotmail.com>
2015-10-12 16:32:12 -0500

    MOD (john)
    se documenta

--- projectCentralizador/projectCentralizador-ejb/src/main/java/co/uniandes/edu/service/Response/RespuestaNotificacion.java
+++ projectCentralizador/projectCentralizador-ejb/src/main/java/co/uniandes/edu/service/Response/RespuestaNotificacion.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.uniandes.edu.service.Response;
 
 import co.uniandes.edu.ecos.dto.NotificacionDto;
@@ -13,23 +8,44 @@ import java.util.List;
  *
  * @author Johans Gonzalez
  */
-public class RespuestaNotificacion extends RespuestaService{
-    
+public class RespuestaNotificacion extends RespuestaService {
+
+    /**
+     * Coleccion de notificaciones.
+     */
     private List<NotificacionDto> notificacionDto;
-    
-    public RespuestaNotificacion(){        
-        this.notificacionDto =  new ArrayList<>();
+
+    /**
+     * Inicializa una nueva instancia.
+     */
+    public RespuestaNotificacion() {
+        this.notificacionDto = new ArrayList<>();
     }
-    
-    public RespuestaNotificacion(List<NotificacionDto> notificacionDto){        
-        this.notificacionDto =  notificacionDto;
+
+    /**
+     * Inicializacion de una nueva instancia con parémetros
+     *
+     * @param notificacionDto
+     */
+    public RespuestaNotificacion(List<NotificacionDto> notificacionDto) {
+        this.notificacionDto = notificacionDto;
     }
-    
+
+    /**
+     * Obtiene la colección de notificaciones.
+     *
+     * @return coleccion de objetos NotificacionDto.
+     */
     public List<NotificacionDto> getNotificacionDto() {
         return this.notificacionDto;
     }
 
+    /**
+     * Establece las notificaciones.
+     *
+     * @param notificacionDto a establecer.
+     */
     public void setNotificacionDto(List<NotificacionDto> notificacionDto) {
         this.notificacionDto = notificacionDto;
-    }    
+    }
 }
